077820ebd3299831495fb5574894a548c7fb604a
Max Zakharzhevskiy <max.za@digitalpersona.com>
2019-11-25 01:37:05 -0800

    Added autocomplete and Google's notes

--- src/index.js
+++ src/index.js
@@ -12,54 +12,85 @@ Vue.component('child', {
 });
 
 const formsData = {
-    formA: {
+    'formA-nn': {
         label: 'Change: New+New',
         fields: [
             {
                 type: 'password',
                 placeholder: "Password New",
+                name: 'password-new',
+                autocomplete: 'new-password',
                 'data-ftype': 1 // new
             },
             {
                 type: 'password',
                 placeholder: "Password Confirm",
+                name: 'password-confirm',
+                autocomplete: 'new-password',
                 'data-ftype': 1 // new
+            },
+            {
+                type: 'hidden',
+                name: 'username',
+                value: 'maxzz',
+                'data-ftype': -1
             },
         ]
     },
-    formB: {
+    'formB-cn': {
         label: 'Change: Cur+New',
         fields: [
             {
                 type: 'password',
                 placeholder: "Password Current",
-                name: 'current-password',
+                name: 'password-current',
+                autocomplete: 'current-password',
                 'data-ftype': 0 // old
             },
             {
                 type: 'password',
                 placeholder: "Password New",
+                name: 'password-new',
+                autocomplete: 'new-password',
                 'data-ftype': 1 // new
+            },
+            {
+                type: 'hidden',
+                name: 'username',
+                value: 'maxzz',
+                'data-ftype': -1
             },
         ]
     },
-    formC: {
+    'formC-cnn': {
         label: 'Change Cur+New+New',
         fields: [
             {
                 type: 'password',
                 placeholder: "Password Current",
+                name: 'password-current', // <- always current
+                autocomplete: 'current-password',
                 'data-ftype': 0 // old
             },
             {
                 type: 'password',
                 placeholder: "Password New",
+                name: 'password-new',
+                autocomplete: 'new-password',
                 'data-ftype': 1 // new
             },
             {
                 type: 'password',
                 placeholder: "Password Confirm",
+                name: 'password-confirm',
+                autocomplete: 'new-password',
                 'data-ftype': 1 // new
+            },
+            {
+                type: 'hidden',
+                name: 'username',
+                value: 'maxzz',
+                'data-ftype': -1
             },
         ]
     },
@@ -94,11 +125,11 @@ Vue.component('test-form', {
     },
     watch: {
         opt_hasUsername: function(value) {
-            this.addUsername(value);
+            this.addFormUsername(value);
         }
     },
     methods: {
-        addUsername(add) {
+        addFormUsername(add) {
             let formEl = this.$el.querySelector('.form-username');
             let usernameEl = formEl.querySelector('.username');
             if (add) {
@@ -108,6 +139,7 @@ Vue.component('test-form', {
                         type: 'text',
                         'class': 'username',
                         placeholder: 'User name',
+                        'name': 'username-0',
                         value: 'maxzz'
 
                     });
@@ -130,49 +162,51 @@ Vue.component('test-form', {
         },
         fillValues() {
             let all = [...this.$el.querySelectorAll('.form-fields input')];
-            all.forEach((_, index) => {
-                _.value = `____ ${this.fillin[index]} ____`;
-            });
+            all.forEach((_, index) => _.value = `____ ${this.fillin[index]} ____`);
         },
         clearValues() {
             let all = [...this.$el.querySelectorAll('.form-fields input')];
-            all.forEach(_ => {
-                _.value = '';
-            });
+            all.forEach(_ => _.value = '');
         },
     },
 });
+
+const STORAGE_CURRENT = 'd16-pchange-data';
+const STORAGE_VAULT = 'd16-pchange-vault';
 
 Vue.component('hidden-data-form', {
     template: '#hidden-data-form',
     data: () => {
         return {
             lastIndex: 10,
-            loading: false,
-            fields: []
+            loadingLocalStorage: false,
+            fields: [],
+            vault: [],
         };
     },
     created() {
-        this.loading = true;
-        this.fields = this.loadFields();
+        this.loadLocalStorage();
+        this.loadVault();
     },
     methods: {
-        loadFields() {
-            let cnt = localStorage.getItem('ps-test-data');
+        /* Fields */
+        parseFieldsFromString(cnt) {
             try {
                 cnt = cnt && JSON.parse(cnt) || [];
             } catch(err) {
                 cnt = [];
             }
             cnt.forEach(_ => _.idx = this.lastIndex++);
-
-            console.log('now', cnt.map(_ => _.idx));
-
             return cnt;
         },
-        saveField() {
+        loadLocalStorage() {
+            this.loadingLocalStorage = true;
+            let cnt = localStorage.getItem(STORAGE_CURRENT);
+            this.fields = this.parseFieldsFromString(cnt);
+        },
+        saveFieldsToLocalStorage() {
             let cnt = JSON.stringify(this.fields);
-            localStorage.setItem('ps-test-data', cnt);
+            localStorage.setItem(STORAGE_CURRENT, cnt);
         },
         onAddField() {
             this.fields.push({
@@ -182,20 +216,41 @@ Vue.component('hidden-data-form', {
             });
         },
         onDelField(idx) {
-            console.log('id', idx);
-            console.log('deleted now', this.fields.map(_ => _.idx));
             let newFields = this.fields.filter(_ => _.idx != idx);
-            console.log('deleted aft', newFields.map(_ => _.idx));
             this.fields = newFields; 
+        },
+        /* Vault */
+        loadVault() {
+            let vault = localStorage.getItem(STORAGE_VAULT);
+            vault = JSON.parse(vault || []);
+            vault.forEach(_ => _.idx = this.lastIndex++);
+            this.vault = vault;
+        },
+        vaultItemDispText(vaultItem) {
+            let cnt = JSON.parse(vaultItem || '[]');
+            return cnt.reduce((acc, _) => acc += `${_.value}${_.password?'***':''} `, '');
+        },
+        onAddToVault() {
+            this.vault = [{
+                data: JSON.stringify(this.fields),
+                idx: this.lastIndex++,
+            }, ...this.vault];
+            localStorage.setItem(STORAGE_VAULT, JSON.stringify(this.vault));
+        },
+        onDelFromVault(idx) {
+            this.vault = this.vault.filter(_ => _.idx !== idx);
+        },
+        onSetFieldsFromVault(data) {
+            this.fields = this.parseFieldsFromString(data);
         },
     },
     watch: {
         fields: {
             handler(val, old) {
-                if (this.loading) {
-                    this.loading = false;
+                if (this.loadingLocalStorage) {
+                    this.loadingLocalStorage = false;
                 } else {
-                    this.saveField();
+                    this.saveFieldsToLocalStorage();
                 }
             },
             deep: true
