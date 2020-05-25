86c7891ba9720e6b8722cbcc75eac6743802fa02
Max Zakharzhevskiy <max.za@digitalpersona.com>
2020-05-24 19:10:20 -0700

    Get rid of old events example: after

--- src/index.js
+++ src/index.js
@@ -301,24 +301,9 @@ Vue.component('data-forms', {
 
 var vm = new Vue({
     el: '#app',
-    created() {
-        this.$on('event_parent', function(id) {
-            console.log(`Event from parent component emitted '${id}'`);
-        });
-    },
-    data: function() {
-        return {
-            msg: 'hello vue',
-            id: ''
-        };
-    },
-    methods: {
-        eventChild: function(id) {
-            console.log(`Event from child component emitted '${id}'`);
-        },
-        eventParent: function() {
-            this.$emit('event_parent', 1);
-        }
+    setup() {
+        console.log('start');
+        return {};
     }
 });
 
